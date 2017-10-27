e61973f48e7ba237cd98e578115f94d78b202aa9
rajamettu <rajasekhar.mettu@gmail.com>
2017-10-27 10:41:45 -0400

    Add hit score (#39)
    * add_hit_score: adding hit score to get individual hit score
    
    * Adding Styling to Score and Data Persistence
    
    * Adding Reset Button

--- client/js/src/gameObject.js
+++ client/js/src/gameObject.js
@@ -3,28 +3,58 @@
 window.mjolnirGameObject = {
     score: 0,
 
-    levels : [{
-    	background: "assets/textures/iceground.jpg",
-    	ground: "assets/textures/snow.jpg"
+    levels: [{
+        background: "assets/textures/iceground.jpg",
+        ground: "assets/textures/snow.jpg"
     },
     {
-    	background: "assets/textures/alien_background2.jpg",
-    	ground: "assets/textures/ground_texture.jpg"
+        background: "assets/textures/alien_background2.jpg",
+        ground: "assets/textures/ground_texture.jpg"
     }
-	]
+    ]
 };
 
 window.addEventListener('targetHit', function (event) {
-  console.log(event.detail.target + ' Wasted');
-  window.mjolnirGameObject.score += event.detail.score;
-  updateScore(window.mjolnirGameObject.score);
+    console.log(event.detail.target + ' Wasted');
+    window.mjolnirGameObject.score += event.detail.score;
+
+    var scoreData = {
+        totalScore: window.mjolnirGameObject.score
+    };
+
+    localStorage.setItem('mjs-score', JSON.stringify(scoreData));
+
+    updateScore(event.detail.score);
 });
 
-function updateScore(score) {
-  document.querySelector('.score-board span').textContent = score;
+function updateScore(hit_score) {
+    document.querySelector('.hit-score span').textContent = hit_score || 0;
+    document.querySelector('.score-board span').textContent = window.mjolnirGameObject.score;
 }
 
-function getCurrentLevel () {
-	var searchParams = new URLSearchParams(window.location.search);
-	return searchParams.get('level');
+function resetGame() {
+    localStorage.removeItem('mjs-score');
+    window.location.reload();
 }
+
+function getCurrentLevel() {
+    var searchParams = new URLSearchParams(window.location.search);
+    return searchParams.get('level');
+}
+
+function getCurrentLevel() {
+    var searchParams = new URLSearchParams(window.location.search);
+    return searchParams.get('level');
+}
+
+function scoreInit() {
+    var scoreData = localStorage.getItem('mjs-score');
+    console.log(scoreData);
+    if (scoreData) {
+        scoreData = JSON.parse(scoreData);
+        window.mjolnirGameObject.score = scoreData.totalScore;
+        updateScore();
+    }
+}
+
+scoreInit();
--- client/js/src/handleInput.js
+++ client/js/src/handleInput.js
@@ -1,37 +1,37 @@
-var handleMouseClick = function (event) {
-        var velocity = 35 // TODO: Temporary value for testing
+var handleMouseClick = function(event) {
+    var velocity = 35 // TODO: Temporary value for testing
 
-        var inputValues = {
-            x: event.clientX,
-            y: event.clientY,
-            velocity: velocity
-        };
+    var inputValues = {
+        x: event.clientX,
+        y: event.clientY,
+        velocity: velocity
+    };
 
-        var event = new CustomEvent('generateHammer', { detail: {inputValues: inputValues} });
-        window.dispatchEvent(event);
+    var event = new CustomEvent('generateHammer', { detail: { inputValues: inputValues } });
+    window.dispatchEvent(event);
 };
 //making this global variable so that I can access it in setupInputSources
 var webcamMotionInstance = null;
-var handleWebCamMotion = function () {
+var handleWebCamMotion = function() {
     var webcamConfig = { video: "#vid", canvas1: "#canvas", canvas2: "#canvas2", fps: 30 };
-    if(webcamMotionInstance === null) {
-        webcamMotionInstance = new WebCamMotion(webcamConfig, function (x, y) {
+    if (webcamMotionInstance === null) {
+        webcamMotionInstance = new WebCamMotion(webcamConfig, function(x, y) {
             console.log({ x: x, y: y });
             var velocity = 35 // TODO: Temporary value for testing
 
             var inputValues = {
-                x: x*6,
-                y: y*4.5,
+                x: x * 6,
+                y: y * 4.5,
                 velocity: velocity
             };
 
-            var event = new CustomEvent('generateHammer', { detail: {inputValues: inputValues} });
+            var event = new CustomEvent('generateHammer', { detail: { inputValues: inputValues } });
             window.dispatchEvent(event);
         });
     }
 };
 
-var setupInputSources = function (event) {
+var setupInputSources = function(event) {
 
     //remove the listener before setting it up again
     window.removeEventListener('mousedown', handleMouseClick);
@@ -41,8 +41,8 @@ var setupInputSources = function (event) {
 
     var isMouse = document.querySelector("input[name='hammerSource']:checked").value === 'mouse';
 
-    if ( isMouse ) {
-        window.addEventListener( 'mousedown', handleMouseClick);
+    if (isMouse) {
+        window.addEventListener('mousedown', handleMouseClick);
     } else {
         handleWebCamMotion();
     }
@@ -53,21 +53,19 @@ setupInputSources();
 
 //run the setup after page load, good for re setting up the source
 var radioButtons = document.querySelectorAll("input[name='hammerSource']");
-Array.from(radioButtons).forEach(function (radioButton) {
-    radioButton.addEventListener('mousedown', function (event) {
+Array.from(radioButtons).forEach(function(radioButton) {
+    radioButton.addEventListener('mousedown', function(event) {
         event.target.checked = true;
         event.stopPropagation();
         setupInputSources();
     });
 })
 
-
 var links = document.querySelectorAll('.level-link');
 
-links.forEach(function (link) {
-	link.addEventListener('click', function(e) {
-		e.preventDefault();
-		window.location.search = '?level=' + event.target.getAttribute('data-level');
-	});
+links.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        window.location.search = '?level=' + event.target.getAttribute('data-level');
+    });
 });
-
