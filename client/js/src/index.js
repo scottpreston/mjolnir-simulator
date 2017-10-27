7be43f98355ae3a387b5127743352d0cd62df46b
calebeno <caleb.eno@gmail.com>
2017-10-27 13:52:50 -0400

    Making Levels Modular (#41)
    - Added levels 1, 2, and 3

--- client/js/src/index.js
+++ client/js/src/index.js
@@ -37,7 +37,7 @@ var transformAux1 = new Ammo.btTransform();
 var tempBtVec3_1 = new Ammo.btVector3( 0, 0, 0 );
 
 var time = 0;
-var currentLevel = getCurrentLevel();
+var currentLevel = getCurrentLevel() - 1;  // zero based level number for internal use
 
 var objectsToRemove = [];
 for ( var i = 0; i < 500; i++ ) {
@@ -213,32 +213,48 @@ function createTargetPane(xpos, ypos, zpos, targetData) {
 }
 
 function createObjects() {
+    switch (currentLevel + 1) {
+        case 1:
+            createLevel1Objects();
+            break;
 
-	// Ground
-	pos.set( 0, - 0.5, 0 );
-	quat.set( 0, 0, 0, 1 );
-	var ground = createParalellepipedWithPhysics( 100, 1, 100, 0, pos, quat, new THREE.MeshPhongMaterial( { color: 0xFFFFFF } ) );
-	ground.receiveShadow = true;
-	textureLoader.load( window.mjolnirGameObject.levels[currentLevel].ground, function( texture ) {
-		texture.wrapS = THREE.RepeatWrapping;
-		texture.wrapT = THREE.RepeatWrapping;
-		texture.repeat.set( 3, 2 );
-		ground.material.map = texture;
-		ground.material.needsUpdate = true;
-	} );
+        case 2:
+            createLevel2Objects();
+            break;
 
-	var hydraTargetData = {type: 'hydra', score: 10};
-	var ultronTargetData = {type: 'ultron', score: 20};
-	var lokiTargetData = {type: 'loki', score: 30};
-	var shieldTargetData = {type: 'shield', score: -20};
-	var thanosTargetData = {type: 'thanos', score: 80};
+        case 3:
+            createLevel3Objects();
+            break;
 
-	// Add Hydra Panes
-	createTargetPane(20, .5, -15, hydraTargetData);
-	createTargetPane(-20, 15, -20, ultronTargetData);
-	createTargetPane(-10, 0, 5, lokiTargetData);
-	createTargetPane(0, 3, -17, shieldTargetData);
-	createTargetPane(7, 10, 0, thanosTargetData);
+        default:
+            createLevel1Objects();
+    }
+
+	// // Ground
+	// pos.set( 0, - 0.5, 0 );
+	// quat.set( 0, 0, 0, 1 );
+	// var ground = createParalellepipedWithPhysics( 100, 1, 100, 0, pos, quat, new THREE.MeshPhongMaterial( { color: 0xFFFFFF } ) );
+	// ground.receiveShadow = true;
+	// textureLoader.load( window.mjolnirGameObject.levels[currentLevel].ground, function( texture ) {
+	// 	texture.wrapS = THREE.RepeatWrapping;
+	// 	texture.wrapT = THREE.RepeatWrapping;
+	// 	texture.repeat.set( 3, 2 );
+	// 	ground.material.map = texture;
+	// 	ground.material.needsUpdate = true;
+	// } );
+
+	// var hydraTargetData = {type: 'hydra', score: 10};
+	// var ultronTargetData = {type: 'ultron', score: 20};
+	// var lokiTargetData = {type: 'loki', score: 30};
+	// var shieldTargetData = {type: 'shield', score: -20};
+	// var thanosTargetData = {type: 'thanos', score: 80};
+
+	// // Add Hydra Panes
+	// createTargetPane(20, .5, -15, hydraTargetData);
+	// createTargetPane(-20, 15, -20, ultronTargetData);
+	// createTargetPane(-10, 0, 5, lokiTargetData);
+	// createTargetPane(0, 3, -17, shieldTargetData);
+	// createTargetPane(7, 10, 0, thanosTargetData);
 }
 
 function createParalellepipedWithPhysics( sx, sy, sz, mass, pos, quat, material ) {
