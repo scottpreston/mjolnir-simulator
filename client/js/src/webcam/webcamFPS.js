f520857478ebe2ced6ba815d20d21a558934777b
Scott Preston <me@scottpreston.org>
2018-01-06 14:31:15 -0500

    modified FPS test

--- client/js/src/webcam/webcamFPS.js
+++ client/js/src/webcam/webcamFPS.js
@@ -1,19 +1,11 @@
 var webcamInterval;
-var MyFPS = 30;
-var hitTime = new Date().getTime();
-var previousImageData = new Array(MyFPS);
-var imageIndex = 0;
-var timeCapture = 0;
-var frameCount = 0;
-var startTime = new Date().getTime();
-var initialTime = new Date().getTime();
-var droppedFrames = 0, decodedFrames  = 0;
+var decodedFrames  = 0;
 
 function WebCamFPS(options, callback) {
-
     console.log(navigator.mediaDevices.getSupportedConstraints())
+
     var self = this;
-    this.fps = 1000 / MyFPS;
+    this.fps = 1000 / options.fps;
     this.video = document.querySelector(options.video);
     this.canvas = document.querySelector(options.canvas1);
     this.ctx = canvas.getContext('2d');
@@ -22,9 +14,11 @@ function WebCamFPS(options, callback) {
     this.processors = [];
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
     window.URL = window.URL || window.webkitURL;
+
     var qvgaConstraints = {
         video: { width: { exact: options.width }, height: { exact: options.height }, frameRate: options.fps }
     };
+
     navigator.getUserMedia(qvgaConstraints, function (stream) {
         self.video.src = window.URL.createObjectURL(stream);
         localMediaStream = stream;
@@ -34,7 +28,6 @@ function WebCamFPS(options, callback) {
     });
     webcamInterval = setInterval(function () {
         self.ctx.drawImage(self.video, 0, 0);
-        //populateImageData(self.ctx);
     }, self.fps);
 
     setInterval(function() {
